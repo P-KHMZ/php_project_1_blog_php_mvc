51758737bc4e2494e42efc1e27f67cffb7ff25ff
User <chiruzabisimwa@gmail.com>
2021-11-14 09:07:24 +0200

    Init undestanding

--- js/editor.js
+++ js/editor.js
@@ -1,24 +1,24 @@
-function checkTitle(event)
-{
-    var title   = document.querySelector("input[name='title']");
+
+function checkTitle(event) {
+    var title = document.querySelector("input[name='title']");
     var warning = document.querySelector("form #title-warning");
-    var label   = document.querySelector("label");
+    var label = document.querySelector("label");
     var form_title = document.querySelector("form #form_title")
-    if(title.value ==="")
-    {
+
+    if (title.value === "") {
         //prevent deafault, ie don't submit the form
         event.preventDefault();
         //display a warning
         warning.innerHTML = "*You must write a title for the entry";
-    } 
+    }
 }
+// Thank you for you comments
 
-function init()
-{
+function init() {
     var editorForm = document.querySelector("form#editor");
-    var title   = document.querySelector("input[name='title']");
+    var title = document.querySelector("input[name='title']");
     title.required = false;
     editorForm.addEventListener("submit", checkTitle, false);
-    
 }
+
 document.addEventListener("DOMContentLoaded", init, false);
