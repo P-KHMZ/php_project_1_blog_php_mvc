da46c9ba90560811a3485eb042d8c1e86308b41f
Peter404 <pierrekahumuza@outlook.com>
2021-11-16 06:13:21 -0600

    Search buttons reply message

--- views/search-form-results-html.php
+++ views/search-form-results-html.php
@@ -5,8 +5,9 @@
     {
         trigger_error('views/search-results-html.php requires a $search_data_found');
     }
-    $search_html = "<section id='search'><p>
-                    You Searched for <em>$search_term</em></p><ul>";
+    $search_html = "<section id='search'>
+                    <p>You Searched for <em>$search_term</em></p>
+                    <p>Here is the result of what you are looking for: </p><ul>";
                     if($search_row === false)
                         {
                             $search_html .="<p>No entries match your search </p>";
